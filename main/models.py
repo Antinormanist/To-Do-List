554f4ec4e906a139c3039dd670716b3d69c30117
Antinormanist <katya.goryacheva.2009@gmail.com>
2024-07-01 16:31:31 +1000

    Added user registration, authenticationg and logout. User can create task

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Note(models.Model):
+    name = models.CharField(max_length=12, default='Note')
+    text = models.TextField(default='Help my mom')
+    priority = models.CharField(max_length=9)
+    task_class = models.CharField(max_length=10)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, blank=True, null=True)
+    
+    
+    class Meta:
+        db_table = 'note'
--- main/views.py
+++ main/views.py
@@ -1,8 +1,37 @@
 from django.shortcuts import render
 
+from .forms import CreateTask
+from .models import Note
+
 # Create your views here.
 def index(request):
+    if request.method == 'GET':
+        if not request.user.is_anonymous:
+            text = request.GET.get('text')
+            name = request.GET.get('name')
+            priority = request.GET.get('priorities')
+            if text and name and priority:
+                name = name[:12]
+                if priority == 'common':
+                    task_class = 'priority-e'
+                elif priority == 'medium':
+                    task_class = 'priority-m'
+                else:
+                    task_class = 'priority-h'
+                    
+                Note.objects.create(
+                    text=text,
+                    name=name,
+                    priority=priority,
+                    task_class=task_class,
+                    user=request.user
+                )
+        
+    
+    
     context = {
         'title': 'Main page',
     }
+    if not request.user.is_anonymous:
+        context['tasks'] = Note.objects.filter(user=request.user)
     return render(request, 'main/index.html', context)
