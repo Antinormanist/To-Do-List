5116e59a682787dc2e2ddbefea3906266d2e5c8f
Antinormanist <katya.goryacheva.2009@gmail.com>
2024-07-03 07:01:38 +1000

    Ready to deploy

--- ToDoList/settings.py
+++ ToDoList/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u73v*#gagwzjf)9!8&$*hey&ajps#zpbv3vm=ua(mr*=of8*71'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.10:8000']
 
 
 # Application definition
